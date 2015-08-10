43d849d55491dcff9ada1cea894e51b470d0c694
Kyle Lacy <kylewlacy@me.com>
2015-08-10 15:46:40 -0700

    Add `BufferBits` bitflags

--- src/types.rs
+++ src/types.rs
@@ -1,3 +1,5 @@
+use super::gl;
+
 pub struct Color {
     pub r: f32,
     pub g: f32,
@@ -8,5 +10,13 @@ pub struct Color {
 impl Color {
     pub fn rgba(r: f32, g: f32, b: f32, a: f32) -> Self {
         Color { r: r, g: g, b: b, a: a}
+    }
+}
+
+bitflags! {
+    flags BufferBits: gl::types::GLbitfield {
+        const COLOR_BUFFER_BIT = gl::COLOR_BUFFER_BIT,
+        const DEPTH_BUFFER_BIT = gl::DEPTH_BUFFER_BIT,
+        const STENCIL_BUFFER_BIT = gl::STENCIL_BUFFER_BIT
     }
 }
