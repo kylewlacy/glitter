fa8f49e7cb3a751880a92b244214f5b1032f6d97
Kyle Lacy <kylewlacy@me.com>
2015-08-11 16:50:35 -0700

    Add `Context.clear` and `clear_color`

--- src/context.rs
+++ src/context.rs
@@ -1,7 +1,21 @@
+use super::gl;
+
 pub struct Context;
 
 impl Context {
     pub unsafe fn current_context() -> Self {
         Context
+    }
+
+    pub fn clear_color(&mut self, color: super::Color) {
+        unsafe {
+            gl::ClearColor(color.r, color.g, color.b, color.a);
+        }
+    }
+
+    pub fn clear(&mut self, buffers: super::BufferBits) {
+        unsafe {
+            gl::Clear(buffers.bits())
+        }
     }
 }
