76f92581d90123136ff133eb934de798edf900a4
Kyle Lacy <kylewlacy@me.com>
2015-08-12 12:42:10 -0700

    Add `Context.gen_buffer`

--- src/context.rs
+++ src/context.rs
@@ -1,4 +1,7 @@
+use std::mem;
 use super::gl;
+use super::gl_lib::types::*;
+use super::Buffer;
 
 pub struct Context;
 
@@ -16,6 +19,14 @@ impl Context {
     pub fn clear(&mut self, buffers: super::BufferBits) {
         unsafe {
             gl::Clear(buffers.bits())
+        }
+    }
+
+    pub fn gen_buffer(&self) -> Buffer {
+        unsafe {
+            let mut id : GLuint = mem::uninitialized();
+            gl::GenBuffers(1, &mut id as *mut GLuint);
+            Buffer::from_id(id)
         }
     }
 }
