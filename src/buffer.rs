b91ecf60c9bfdd1c6aea8ec80f52fd3ce5a1f03d
Kyle Lacy <kylewlacy@me.com>
2015-08-12 13:48:27 -0700

    Add buffer binders and bindings

--- src/buffer.rs
+++ src/buffer.rs
@@ -1,3 +1,5 @@
+use std::mem;
+use std::marker::PhantomData;
 use super::gl;
 use super::gl_lib::types::*;
 
@@ -16,5 +18,89 @@ impl Drop for Buffer {
         unsafe {
             gl::DeleteBuffers(1, &self.gl_id as *const GLuint);
         }
+    }
+}
+
+
+
+#[derive(Debug, Clone, Copy)]
+#[allow(non_camel_case_types)]
+pub enum BufferDataUsage {
+    STREAM_DRAW = gl::STREAM_DRAW as isize,
+    STATIC_DRAW = gl::STATIC_DRAW as isize,
+    DYNAMIC_DRAW = gl::DYNAMIC_DRAW as isize
+}
+pub use self::BufferDataUsage::*;
+
+#[derive(Debug, Clone, Copy)]
+#[allow(non_camel_case_types)]
+pub enum BufferBindingTarget {
+    ARRAY_BUFFER = gl::ARRAY_BUFFER as isize,
+    ELEMENT_ARRAY_BUFFER = gl::ELEMENT_ARRAY_BUFFER as isize
+}
+pub use self::BufferBindingTarget::*;
+
+
+
+pub trait BufferBinding {
+    fn target(&self) -> BufferBindingTarget;
+
+    fn buffer_bytes(&mut self, bytes: &[u8], usage: BufferDataUsage) {
+        let ptr = bytes.as_ptr();
+        let size = bytes.len() * mem::size_of::<u8>();
+        unsafe {
+            gl::BufferData(self.target() as GLenum,
+                           size as GLsizeiptr,
+                           ptr as *const GLvoid,
+                           usage as GLenum)
+        }
+    }
+}
+
+pub struct ArrayBufferBinding<'a> {
+    phantom: PhantomData<&'a mut Buffer>
+}
+
+impl<'a> BufferBinding for ArrayBufferBinding<'a> {
+    fn target(&self) -> BufferBindingTarget {
+        ARRAY_BUFFER
+    }
+}
+
+pub struct ElementArrayBufferBinding<'a> {
+    phantom: PhantomData<&'a mut Buffer>
+}
+
+impl<'a> BufferBinding for ElementArrayBufferBinding<'a> {
+    fn target(&self) -> BufferBindingTarget {
+        ELEMENT_ARRAY_BUFFER
+    }
+}
+
+
+
+pub struct ArrayBufferBinder;
+impl ArrayBufferBinder {
+    pub fn bind<'a>(&'a mut self, buffer: &mut Buffer)
+        -> ArrayBufferBinding<'a>
+    {
+        let binding = ArrayBufferBinding { phantom: PhantomData };
+        unsafe {
+            gl::BindBuffer(binding.target() as GLuint, buffer.gl_id);
+        }
+        binding
+    }
+}
+
+pub struct ElementArrayBufferBinder;
+impl ElementArrayBufferBinder {
+    pub fn bind<'a>(&'a mut self, buffer: &mut Buffer)
+        -> ElementArrayBufferBinding<'a>
+    {
+        let binding = ElementArrayBufferBinding { phantom: PhantomData };
+        unsafe {
+            gl::BindBuffer(binding.target() as GLuint, buffer.gl_id);
+        }
+        binding
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,9 @@ mod types;
 pub use gl_lib as gl;
 
 pub use context::Context;
-pub use buffer::Buffer;
+pub use buffer::{Buffer, BufferDataUsage, BufferBinding,
+                 STREAM_DRAW, STATIC_DRAW, DYNAMIC_DRAW,
+                 ArrayBufferBinder, ElementArrayBufferBinder,
+                 ArrayBufferBinding, ElementArrayBufferBinding};
 pub use types::{Color, BufferBits,
                 COLOR_BUFFER_BIT, DEPTH_BUFFER_BIT, STENCIL_BUFFER_BIT};
