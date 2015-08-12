9cb6d87b5f27e008151c05b964510d7d030c4377
Kyle Lacy <kylewlacy@me.com>
2015-08-12 12:16:53 -0700

    Change how `gl` is exported (again)

--- src/lib.rs
+++ src/lib.rs
@@ -4,9 +4,7 @@ extern crate gl as gl_lib;
 mod context;
 mod types;
 
-pub mod gl {
-    pub use super::gl_lib::*;
-}
+pub use gl_lib as gl;
 
 pub use context::Context;
 pub use types::{Color, BufferBits,
