13d6d8ac1ed03480576d86a505468f24c81a794d
Kyle Lacy <kylewlacy@me.com>
2015-08-18 17:23:17 -0700

    Add `Program.link()`

--- src/program.rs
+++ src/program.rs
@@ -1,5 +1,8 @@
+use std::mem;
+use std::ptr;
 use super::gl;
 use super::gl_lib::types::*;
+use super::GLError;
 use super::Shader;
 
 pub struct Program {
@@ -14,6 +17,43 @@ impl Program {
     pub fn attach_shader(&mut self, shader: &Shader) {
         unsafe {
             gl::AttachShader(self.gl_id, shader.gl_id());
+        }
+    }
+
+    pub fn link(&self) -> Result<(), GLError> {
+        let success = unsafe {
+            gl::LinkProgram(self.gl_id);
+            let mut link_status : GLint = mem::uninitialized();
+            gl::GetProgramiv(self.gl_id,
+                             gl::LINK_STATUS,
+                             &mut link_status as *mut GLint);
+
+            link_status == gl::TRUE as GLint
+        };
+
+        if success {
+            Ok(())
+        }
+        else {
+            unsafe {
+                let mut info_length : GLint = mem::uninitialized();
+                gl::GetProgramiv(self.gl_id,
+                                 gl::INFO_LOG_LENGTH,
+                                 &mut info_length as *mut GLint);
+
+                let mut bytes = Vec::<u8>::with_capacity(info_length as usize);
+
+                gl::GetProgramInfoLog(self.gl_id,
+                                      info_length,
+                                      ptr::null_mut(),
+                                      bytes.as_mut_ptr() as *mut GLchar);
+                bytes.set_len((info_length - 1) as usize);
+
+                let msg = String::from_utf8(bytes)
+                                 .unwrap_or(String::from("<Unknown error>"));
+
+                Err(GLError { message: msg })
+            }
         }
     }
 }
