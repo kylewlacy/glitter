6100e0b7b688f611742aec76df42414dc4dc19fc
Kyle Lacy <kylewlacy@me.com>
2015-08-20 16:49:10 -0700

    Rework `vertex_data!` macro

--- src/vertex_data.rs
+++ src/vertex_data.rs
@@ -5,14 +5,14 @@ use std::raw;
 
 pub trait VertexData: Copy {
     type Binder: VertexAttribBinder;
+
+    fn build_attrib_binder() -> <Self::Binder as VertexAttribBinder>::Builder;
 }
 
 pub trait VertexAttribBinder {
     type Builder;
 
-    fn build() -> Self::Builder;
-
-    fn bind(&mut self, gl: Context);
+    fn bind(&self, gl: &Context);
 }
 
 #[allow(non_camel_case_types)]
@@ -101,8 +101,36 @@ impl<T> VertexBytes for T where T: VertexData {
     }
 }
 
+impl<T> VertexBytes for [T] where T: VertexData {
+    fn vertex_bytes(&self) -> &[u8] {
+        unsafe {
+            mem::transmute(raw::Slice::<T> {
+                data: &self[0],
+                len: mem::size_of::<T>() * self.len()
+            })
+        }
+    }
+}
+
+// TODO: Use a proper compiler intrinsic/macro (when available)
+// see: https://github.com/rust-lang/rust/issues/24111
+#[macro_export]
+macro_rules! offset_of {
+    ($T:ty, $field:ident) => {
+        unsafe {
+            use std::mem::{uninitialized, transmute};
+
+            let obj : $T = uninitialized();
+            let obj_ptr : *const u8 = transmute(&obj);
+            let member_ptr : *const u8 = transmute(&obj.$field);
+
+            (member_ptr as usize) - (obj_ptr as usize)
+        }
+    }
+}
 
 
+#[macro_use(offset_of)]
 #[macro_export]
 macro_rules! vertex_data {
     ($(
@@ -110,23 +138,92 @@ macro_rules! vertex_data {
             $($field_name:ident: $field_type:ty),*
         }
     )+) => {
-        mod _glitter_vertex_data {
-            $(
-                #[allow(non_snake_case)]
-                pub mod $name {
-                    #[repr(C)]
-                    #[derive(Debug, Clone, Copy)]
-                    pub struct $name {
-                        $($field_name: $field_type),*
-                    }
+        mod _glitter_vertex_data {$(
+            #[allow(non_snake_case)]
+            pub mod $name {
+                #[repr(C)]
+                #[derive(Debug, Clone, Copy)]
+                pub struct $name {
+                    $(pub $field_name: $field_type),*
+                }
 
-                    pub struct Binder;
+                impl $crate::VertexData for $name {
+                    type Binder = Binder;
 
-                    impl $crate::VertexData for $name {
-                        type Binder = Binder;
+                    fn build_attrib_binder() -> BinderBuilder {
+                        BinderBuilder::new()
                     }
                 }
-            )+
+
+                #[allow(dead_code)]
+                pub struct Binder {
+                    $($field_name: $crate::ProgramAttrib),*
+                }
+
+                impl $crate::VertexAttribBinder for Binder {
+                    type Builder = BinderBuilder;
+
+                    fn bind(&self, gl: &$crate::Context) {
+                        use std::mem;
+                        use $crate::VertexDatum as Datum;
+
+                        $({
+                            let components =
+                                <$field_type as Datum>::components();
+                            let gl_type =
+                              <$field_type as Datum>::gl_type();
+                            let normalized =
+                                <$field_type as Datum>::normalized();
+                            let stride = mem::size_of::<$name>();
+                            let offset = offset_of!($name, $field_name);
+
+                            gl.vertex_attrib_pointer(self.$field_name,
+                                                     components,
+                                                     gl_type,
+                                                     normalized,
+                                                     stride,
+                                                     offset);
+                        };)*
+                    }
+                }
+
+                #[allow(dead_code)]
+                pub struct BinderBuilder {
+                    $($field_name: Option<$crate::ProgramAttrib>),*
+                }
+
+                #[allow(dead_code)]
+                impl BinderBuilder {
+                    pub fn new() -> Self {
+                        BinderBuilder {
+                            $($field_name: None),*
+                        }
+                    }
+
+                    pub fn unwrap(self, gl: &$crate::Context) -> self::Binder {
+                        let binder = self::Binder {
+                            $($field_name: self.$field_name.expect(
+                                concat!("No attribute provided for ",
+                                        stringify!($field_name))
+                            )),*
+                        };
+
+                        $(gl.enable_vertex_attrib_array(binder.$field_name);)*
+
+                        binder
+                    }
+
+                    $(
+                        pub fn $field_name(mut self,
+                                           attrib: $crate::ProgramAttrib)
+                        -> Self
+                        {
+                            self.$field_name = Some(attrib);
+                            self
+                        }
+                    )*
+                }
+            })+
         }
 
 
