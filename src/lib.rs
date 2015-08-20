e81a2f1f3dfd1da87c35d6388d752ddb459600b7
Kyle Lacy <kylewlacy@me.com>
2015-08-20 16:53:08 -0700

    Add `VertexBuffer`

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,7 @@ mod buffer;
 mod shader;
 mod program;
 mod vertex_data;
+mod vertex_buffer;
 mod types;
 
 pub use gl_lib as gl;
@@ -22,5 +23,6 @@ pub use program::{Program, ProgramAttrib};
 pub use vertex_data::{VertexData, VertexDatum, VertexBytes, VertexAttribBinder,
                       DataType, BYTE, UNSIGNED_BYTE, SHORT, UNSIGNED_SHORT,
                       FIXED, FLOAT};
+pub use vertex_buffer::VertexBuffer;
 pub use types::{Color, GLError, BufferBits,
                 COLOR_BUFFER_BIT, DEPTH_BUFFER_BIT, STENCIL_BUFFER_BIT};
---
+++ src/vertex_buffer.rs
@@ -0,0 +1,23 @@
+use std::marker::PhantomData;
+use super::Context;
+use super::{VertexData, VertexBytes, VertexAttribBinder};
+use super::Buffer;
+use super::{BufferBinding, ArrayBufferBinding};
+
+pub struct VertexBuffer<T: VertexData> {
+    pub attrib_binder: Option<T::Binder>,
+    pub buffer: Buffer,
+    count: usize,
+    phantom: PhantomData<*const T>
+}
+
+impl<T: VertexData> VertexBuffer<T> {
+    pub fn new(buffer: Buffer) -> Self {
+        VertexBuffer {
+            attrib_binder: None,
+            buffer: buffer,
+            count: 0,
+            phantom: PhantomData
+        }
+    }
+}
