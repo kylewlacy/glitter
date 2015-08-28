009e6b391d6a4d8b2f34abaf4868da61d07638a1
Kyle Lacy <kylewlacy@me.com>
2015-08-28 16:51:29 -0700

    Add `Context.viewport()`

--- src/context.rs
+++ src/context.rs
@@ -1,6 +1,8 @@
 use super::gl_lib as gl;
+use super::gl_lib::types::*;
+use super::types::{Color, Viewport, BufferBits};
 use super::buffer::{ArrayBufferBinder, ElementArrayBufferBinder};
-use super::program::{ProgramBinder};
+use super::program::{ProgramBinder, ProgramAttrib};
 
 pub struct Context {
     pub array_buffer: ArrayBufferBinder,
@@ -17,21 +19,30 @@ impl Context {
         }
     }
 
-    pub fn clear_color(&mut self, color: super::Color) {
+    pub fn clear_color(&mut self, color: Color) {
         unsafe {
             gl::ClearColor(color.r, color.g, color.b, color.a);
         }
     }
 
-    pub fn clear(&mut self, buffers: super::BufferBits) {
+    pub fn clear(&mut self, buffers: BufferBits) {
         unsafe {
             gl::Clear(buffers.bits())
         }
     }
 
-    pub fn enable_vertex_attrib_array(&self, attrib: super::ProgramAttrib) {
+    pub fn enable_vertex_attrib_array(&self, attrib: ProgramAttrib) {
         unsafe {
             gl::EnableVertexAttribArray(attrib.gl_index);
+        }
+    }
+
+    pub fn viewport(&self, viewport: Viewport) {
+        unsafe {
+            gl::Viewport(viewport.x as GLint,
+                         viewport.y as GLint,
+                         viewport.width as GLsizei,
+                         viewport.height as GLsizei);
         }
     }
 }
