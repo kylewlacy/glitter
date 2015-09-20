0ef4dc4d1157728e4baa915f1a4288e1ac6fe499
Kyle Lacy <kylewlacy@me.com>
2015-09-20 04:47:18 -0700

    Add `TextureBindingTarget` enum

--- src/texture.rs
+++ src/texture.rs
@@ -1,5 +1,13 @@
+use gl;
 use gl::types::*;
 
 pub trait ImageTargetType {
     fn gl_enum(&self) -> GLenum;
+}
+
+gl_enum! {
+    pub gl_enum TextureBindingTarget {
+        Texture2d as TEXTURE_2D = gl::TEXTURE_2D,
+        TextureCubeMap as TEXTURE_CUBE_MAP = gl::TEXTURE_CUBE_MAP
+    }
 }
