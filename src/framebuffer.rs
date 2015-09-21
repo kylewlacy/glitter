fbfd1876a3144b6a844dfbad61aa6a482b99660a
Kyle Lacy <kylewlacy@me.com>
2015-09-21 16:47:01 -0700

    Add `Drop` impl for `Framebuffer`

--- src/framebuffer.rs
+++ src/framebuffer.rs
@@ -1,3 +1,4 @@
+use gl;
 use gl::types::*;
 
 pub struct Framebuffer {
@@ -7,5 +8,13 @@ pub struct Framebuffer {
 impl Framebuffer {
     pub fn gl_id(&self) -> GLuint {
         self.gl_id
+    }
+}
+
+impl Drop for Framebuffer {
+    fn drop(&mut self) {
+        unsafe {
+            gl::DeleteFramebuffers(1, &self.gl_id as *const GLuint);
+        }
     }
 }
