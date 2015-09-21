83e1a1b8056ffb775bd94889f7aa972e50842029
Kyle Lacy <kylewlacy@me.com>
2015-09-21 16:34:04 -0700

    Add `Context.gen_renderbuffer()`

--- src/renderbuffer.rs
+++ src/renderbuffer.rs
@@ -1,5 +1,7 @@
 use gl;
 use gl::types::*;
+use context::Context;
+use types::GLError;
 
 pub struct Renderbuffer {
     gl_id: GLuint
@@ -15,6 +17,24 @@ impl Drop for Renderbuffer {
     fn drop(&mut self) {
         unsafe {
             gl::DeleteRenderbuffers(1, &self.gl_id as *const GLuint);
+        }
+    }
+}
+
+impl Context {
+    pub fn gen_renderbuffer(&self) -> Renderbuffer {
+        unsafe {
+            let mut id : GLuint = 0;
+
+            gl::GenRenderbuffers(1, &mut id as *mut GLuint);
+            dbg_gl_sanity_check! {
+                GLError::InvalidValue => "`n` is negative",
+                _ => "Unknown error"
+            }
+
+            Renderbuffer {
+                gl_id: id
+            }
         }
     }
 }
