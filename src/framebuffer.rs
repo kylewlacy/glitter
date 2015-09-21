0399ff56f3933c37f68a973d6f676aedfe4bf582
Kyle Lacy <kylewlacy@me.com>
2015-09-21 16:50:00 -0700

    Add `Framebuffer::gen_framebuffer()`

--- src/framebuffer.rs
+++ src/framebuffer.rs
@@ -1,5 +1,7 @@
 use gl;
 use gl::types::*;
+use context::Context;
+use types::GLError;
 
 pub struct Framebuffer {
     gl_id: GLuint
@@ -15,6 +17,24 @@ impl Drop for Framebuffer {
     fn drop(&mut self) {
         unsafe {
             gl::DeleteFramebuffers(1, &self.gl_id as *const GLuint);
+        }
+    }
+}
+
+impl Context {
+    pub fn gen_framebuffer(&self) -> Framebuffer {
+        unsafe {
+            let mut id : GLuint = 0;
+
+            gl::GenFramebuffers(1, &mut id as *mut GLuint);
+            dbg_gl_sanity_check! {
+                GLError::InvalidValue => "`n` is negative",
+                _ => "Unknown error"
+            }
+
+            Framebuffer {
+                gl_id: id
+            }
         }
     }
 }
