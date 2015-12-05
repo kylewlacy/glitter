3413078bc4daa893c8feb2da36b04ebc8f7f6dbb
Kyle Lacy <kylewlacy@me.com>
2015-12-04 23:56:15 -0800

    Add basic `RefInto` and `MutInto` implementations

--- src/ref_into.rs
+++ src/ref_into.rs
@@ -5,3 +5,47 @@ pub trait RefInto<'a, T>: MutInto<'a, T> {
 pub trait MutInto<'a, T> {
     fn mut_into(&'a mut self) -> T;
 }
+
+
+
+impl<'a, T: 'a> RefInto<'a, &'a T> for T {
+    fn ref_into(&'a self) -> &'a T {
+        self
+    }
+}
+
+impl<'a, T: 'a> RefInto<'a, &'a T> for &'a T {
+    fn ref_into(&'a self) -> &'a T {
+        &**self
+    }
+}
+
+impl<'a, T: 'a> MutInto<'a, &'a T> for T {
+    fn mut_into(&'a mut self) -> &'a T {
+        self
+    }
+}
+
+impl<'a, T: 'a> MutInto<'a, &'a T> for &'a T {
+    fn mut_into(&'a mut self) -> &'a T {
+        &**self
+    }
+}
+
+impl<'a, T: 'a> MutInto<'a, &'a T> for &'a mut T {
+    fn mut_into(&'a mut self) -> &'a T {
+        &**self
+    }
+}
+
+impl<'a, T: 'a> MutInto<'a, &'a mut T> for T {
+    fn mut_into(&'a mut self) -> &'a mut T {
+        self
+    }
+}
+
+impl<'a, T: 'a> MutInto<'a, &'a mut T> for &'a mut T {
+    fn mut_into(&'a mut self) -> &'a mut T {
+        &mut **self
+    }
+}
