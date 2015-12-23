cae0d64c7051244ca589f51a1f797066632530c9
Kyle Lacy <kylewlacy@me.com>
2015-12-23 13:42:14 -0800

    Add `ContextBufferExt.vertex_attrib_pointer()`

--- src/buffer.rs
+++ src/buffer.rs
@@ -83,6 +83,32 @@ pub unsafe trait ContextBufferExt {
             }
         }
     }
+
+    unsafe fn vertex_attrib_pointer(&self,
+                                    attrib: super::ProgramAttrib,
+                                    components: i8,
+                                    gl_type: super::DataType,
+                                    normalized: bool,
+                                    stride: usize,
+                                    offset: usize)
+    {
+        debug_assert!(1 <= components && components <= 4);
+
+        let gl_normalized = if normalized { gl::TRUE } else { gl::FALSE };
+        gl::VertexAttribPointer(attrib.gl_index,
+                                components as GLint,
+                                gl_type.gl_enum(),
+                                gl_normalized,
+                                stride as GLsizei,
+                                offset as *const GLvoid);
+        dbg_gl_error! {
+            GLError::InvalidEnum => "Illegal vertex attribute type",
+            GLError::InvalidValue => "`stride` is negative, `size` is not in range, or `index` is >= GL_MAX_VERTEX_ATTRIBS",
+            GLError::InvalidFramebufferOperation => "Currently bound framebuffer is not framebuffer complete",
+            _ => "Unknown error"
+        }
+    }
+
     unsafe fn draw_arrays_range(&self,
                                 _ab: &ArrayBufferBinding,
                                 mode: DrawingMode,
