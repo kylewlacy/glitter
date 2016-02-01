64c209efb0f3efd80b84f5a09cd5633c6ea6ffd5
Kyle Lacy <kylewlacy@me.com>
2016-02-01 10:44:44 -0800

    Add `ToRef` and `ToMut` impls for `&T` and `&mut T` references

--- src/to_ref.rs
+++ src/to_ref.rs
@@ -9,3 +9,27 @@ pub trait ToMut<'a>: ToRef<'a> {
 
     fn to_mut(&'a mut self) -> Self::Mut;
 }
+
+impl<'a, T: 'a> ToRef<'a> for &'a T {
+    type Ref = &'a T;
+
+    fn to_ref(&'a self) -> Self::Ref {
+        &*self
+    }
+}
+
+impl<'a, T: 'a> ToRef<'a> for &'a mut T {
+    type Ref = &'a T;
+
+    fn to_ref(&'a self) -> Self::Ref {
+        &*self
+    }
+}
+
+impl<'a, T: 'a> ToMut<'a> for &'a mut T {
+    type Mut = &'a mut T;
+
+    fn to_mut(&'a mut self) -> Self::Mut {
+        &mut *self
+    }
+}
