25afe448eb20f37481195b4e38524452b456ad64
Travis CI <>
2016-06-29 10:40:09 +0000

    Update docs for commit c36b2ea

--- docs/implementors/core/ops/trait.Not.js
+++ docs/implementors/core/ops/trait.Not.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['glitter'] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Not.html' title='core::ops::Not'>Not</a> for <a class='struct' href='glitter/types/struct.BufferBits.html' title='glitter::types::BufferBits'>BufferBits</a>",];
+implementors["glitter"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.Not.html' title='core::ops::Not'>Not</a> for <a class='struct' href='glitter/types/struct.BufferBits.html' title='glitter::types::BufferBits'>BufferBits</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- docs/implementors/core/str/pattern/trait.Searcher.js
+++ docs/implementors/core/str/pattern/trait.Searcher.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['glitter'] = [];
+implementors["glitter"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
